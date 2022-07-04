6069d5c5911fd8168043f80de92105e96ce4b930
jkashiwaya <julio.kashi@hotmail.com>
2022-07-04 10:37:55 -0300

    adicionando desafios 3 e 4

---
+++ stage4_exercicio1/script.js
@@ -0,0 +1,14 @@
+let numberOne = prompt ("Digite um numero")
+let numberTwo = prompt ("Digite o segundo numero")
+
+const sum = prompt(numberOne + numberTwo)
+const sub = prompt(numberOne - numberTwo)
+const mult = prompt(numberOne * numberTwo)
+const div = prompt(numberOne / numberTwo)
+const rest = prompt(numberOne % numberTwo)
+
+alert("A soma dos numeros deu: " + sum)
+alert("A subtracao dos numeros deu: " + sub)
+alert("A multiplicacao dos numeros deu: " + mult)
+alert("A divisao dos numeros deu: " + div)
+alert("O resto dos numeros deu: " + rest)
