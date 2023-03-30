88211f939c06276a428cab1f85cadb8b1100f959
jKashiNote <julio.kashi@hotmail.com>
2023-03-30 11:38:16 -0300

    adicionando stage6

---
+++ projeto07/js/router.js
@@ -0,0 +1,27 @@
+export class Router {
+    routes = {}
+
+    add(routeName, page) {
+        this.routes[routeName] = page
+    }
+
+route(event) {
+    event = event || window.event
+    event.preventDefault()
+
+
+    window.history.pushState({}, "", event.target.href)
+    handle()
+}
+
+handle() {
+    const { pathname } = window.location
+    const route = routes[pathname] || routes [404]
+    fetch(route)
+    .then((data) => data.text())
+    .then(html => {
+        document.querySelector('#app').innerHTML = html                
+    })
+}
+
+}
