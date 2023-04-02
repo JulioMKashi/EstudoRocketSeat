98229edbf8c65b3c5908e2560964a433bf2f231e
jKashiNote <julio.kashi@hotmail.com>
2023-04-01 21:58:59 -0300

    arrumando identação

--- projeto07/js/router.js
+++ projeto07/js/router.js
@@ -5,23 +5,23 @@ export class Router {
         this.routes[routeName] = page
     }
 
-route(event) {
-    event = event || window.event
-    event.preventDefault()
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
 
-    window.history.pushState({}, "", event.target.href)
+        window.history.pushState({}, "", event.target.href)
 
-    this.handle()
-}
+        this.handle()
+    }
 
-handle() {
-    const { pathname } = window.location
-    const route = this.routes[pathname] || this.routes[404]
-    fetch(route)
-    .then((data) => data.text())
-    .then(html => {
-        document.querySelector('#app').innerHTML = html                
-    })
-}
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        fetch(route)
+        .then((data) => data.text())
+        .then(html => {
+            document.querySelector('#app').innerHTML = html                
+        })
+    }
 
 }
